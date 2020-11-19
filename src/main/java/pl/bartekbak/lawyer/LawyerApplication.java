21302b5ffc65ebc9b56f4c0c745576a288a8c25e
BartlomiejBak <bakbartlomiej88>
2020-11-19 09:28:01 +0100

    feat: create list-notes.html. Closes #45

--- src/main/java/pl/bartekbak/lawyer/LawyerApplication.java
+++ src/main/java/pl/bartekbak/lawyer/LawyerApplication.java
@@ -6,6 +6,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import pl.bartekbak.lawyer.dao.*;
 import pl.bartekbak.lawyer.entity.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class LawyerApplication {
 
@@ -81,5 +86,65 @@ public class LawyerApplication {
         contactRepository.save(contactB);
         contactRepository.save(contactC);
         contactRepository.save(contactD);
+
+        //populate lawsuit table
+        LawsuitRepository lawsuitRepository = context.getBean(LawsuitRepository.class);
+        Lawsuit lawsuitA = Lawsuit.builder()
+                .caseSide("left")
+                .claimAmount(20000)
+                .name("case 1")
+                .build();
+        Lawsuit lawsuitB = Lawsuit.builder()
+                .caseSide("left")
+                .claimAmount(20000)
+                .name("case 2")
+                .build();
+        Lawsuit lawsuitC = Lawsuit.builder()
+                .caseSide("left")
+                .claimAmount(20000)
+                .name("case 3")
+                .build();
+        Lawsuit lawsuitD = Lawsuit.builder()
+                .caseSide("left")
+                .claimAmount(20000)
+                .name("case 4")
+                .build();
+        lawsuitRepository.save(lawsuitA);
+        lawsuitRepository.save(lawsuitB);
+        lawsuitRepository.save(lawsuitC);
+        lawsuitRepository.save(lawsuitD);
+
+
+        //populate task table
+        TaskRepository taskRepository = context.getBean(TaskRepository.class);
+        Task taskA = Task.builder()
+                .description("very important task 1")
+                .priority(true)
+                .deadline(LocalDateTime.of(2021, 12, 12, 0, 0))
+                .contactList(List.of(contactA, contactB))
+                .build();
+        Task taskB = Task.builder()
+                .description("very important task 2")
+                .priority(true)
+                .deadline(LocalDateTime.of(2021, 11, 12, 0, 0))
+                .contactList(List.of(contactC, contactB))
+                .build();
+        Task taskC = Task.builder()
+                .description("very important task 3")
+                .priority(true)
+                .deadline(LocalDateTime.of(2021, 2, 12, 0, 0))
+                .contactList(List.of(contactA, contactC))
+                .build();
+        Task taskD = Task.builder()
+                .description("very important task 4")
+                .priority(true)
+                .deadline(LocalDateTime.of(2021, 3, 12, 0, 0))
+                .contactList(List.of(contactD, contactB))
+                .build();
+        taskRepository.save(taskA);
+        taskRepository.save(taskB);
+        taskRepository.save(taskC);
+        taskRepository.save(taskD);
+
     }
 }
