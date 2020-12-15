04020ea746fd9f6b6d54d72454fb2f7b95638425
BartlomiejBak <bakbartlomiej88@gmail.com>
2020-12-15 18:45:38 +0100

    feat: add Payment class. Closes #4

---
+++ src/main/java/pl/bartekbak/lawyer/entity/Payment.java
@@ -0,0 +1,27 @@
+package pl.bartekbak.lawyer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @Column(name = "payment_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "payment_value")
+    private double paymentValue;
+    @Column(name = "payment_date")
+    private LocalDate paymentDate;
+
+}
