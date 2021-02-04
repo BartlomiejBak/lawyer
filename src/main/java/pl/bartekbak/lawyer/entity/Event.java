8225ec44469df7619c8abf4cfdb13387fa192a8a
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-04 22:16:39 +0100

    refactor: refactor Event

--- src/main/java/pl/bartekbak/lawyer/entity/Event.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Event.java
@@ -1,9 +1,16 @@
 package pl.bartekbak.lawyer.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "event")
 public class Event {
 
@@ -11,12 +18,16 @@ public class Event {
     @Column(name = "event_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "title")
     private String title;
+
     @Column(name = "date_time")
     private LocalDateTime dateTime;
+
     @Column(name = "description")
     private String description;
+
     @ManyToOne
     @JoinColumn(name = "lawsuit_event_id", referencedColumnName = "case_id")
     private Lawsuit relatedLawsuit;
