792cc3bc35cbd2131d78f4ef746a81c5f4f43814
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-04 22:23:38 +0100

    refactor: refactor Poa

--- src/main/java/pl/bartekbak/lawyer/entity/Poa.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Poa.java
@@ -15,24 +15,33 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Table(name = "poa")
 public class Poa {
+
     @Id
     @Column(name = "poa_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @Column(name = "type")
     private String type;
+
     @Column(name = "payment")
     private String payment;
+
     @Column(name = "kpc")
     private boolean kpc;
+
     @Column(name = "termination")
     private boolean termination;
+
     @Column(name = "start_date")
     private LocalDate startDate;
+
     @Column(name = "end_date")
     private LocalDate endDate;
+
     @Column(name = "notification_duty")
     private boolean terminationNotificationDuty;
+
     @Column(name = "duty_completed")
     private boolean terminationNotificationDutyCompleted;
 }
