58439831789bd49ea59675b322b34bf4423b08bb
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-04 22:18:04 +0100

    refactor: refactor Lawsuit

--- src/main/java/pl/bartekbak/lawyer/entity/Lawsuit.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Lawsuit.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -22,44 +24,54 @@ public class Lawsuit {
     @Column(name = "case_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "cas_side")
     private String caseSide;
+
     @Column(name = "input_date")
     private LocalDate inputDate;
+
     @Column(name = "deadline")
     private LocalDate deadline;
+
     @Column(name = "signature")
     private String signature;
+
     @Column(name = "claim_amount")
     private double claimAmount;
+
     @Column(name = "add_info")
     @Lob
     private String additionalInfo;
 
     @ManyToMany(mappedBy = "lawsuitList")
-    private List<Contact> contactList;
+    @Builder.Default
+    private List<Contact> contactList = new ArrayList<>();
+
     @ManyToMany
     @JoinTable(
             name = "lawsuit_task",
             joinColumns = @JoinColumn(name = "lawsuit_id"),
             inverseJoinColumns = @JoinColumn(name = "task_id")
     )
-    private List<Task> taskList;
+    @Builder.Default
+    private List<Task> taskList = new ArrayList<>();
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "lawsuit_id_plaintiff")
-    private List<Contact> plaintiff;
+    @Builder.Default
+    private List<Contact> plaintiff = new ArrayList<>();
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "lawsuit_id_defendant")
-    private List<Contact> defendant;
+    @Builder.Default
+    private List<Contact> defendant = new ArrayList<>();
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "lawsuit_id_event")
-    private Set<Event> eventSet;
-
-    public Lawsuit(int id, String name, String caseSide) {
-        this.id = id;
-        this.name = name;
-        this.caseSide = caseSide;
-    }
+    @Builder.Default
+    private Set<Event> eventSet = new HashSet<>();
 }
