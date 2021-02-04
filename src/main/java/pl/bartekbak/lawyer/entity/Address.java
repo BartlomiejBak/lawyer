4620cb10f9484d6f13226422db436d1b976e8535
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-04 22:02:26 +0100

    refactor: refactor Address.java

--- src/main/java/pl/bartekbak/lawyer/entity/Address.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Address.java
@@ -16,19 +16,17 @@ public class Address {
     @Column(name = "address_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "street")
     private String street;
+
     @Column(name = "zip_code")
     private String zipCode;
+
     @Column(name = "city")
     private String city;
+
     @Column(name = "country")
     private String country;
 
-    public Address(String street, String zipCode, String city, String country) {
-        this.street = street;
-        this.zipCode = zipCode;
-        this.city = city;
-        this.country = country;
-    }
 }
