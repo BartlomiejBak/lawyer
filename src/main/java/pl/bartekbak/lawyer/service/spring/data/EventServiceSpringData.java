1cda3e374c10f9336a0694d41138274c8dfd4442
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-05 08:24:06 +0100

    feat: add EventServiceSpringData, closes #11

---
+++ src/main/java/pl/bartekbak/lawyer/service/spring/data/EventServiceSpringData.java
@@ -0,0 +1,44 @@
+package pl.bartekbak.lawyer.service.spring.data;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.bartekbak.lawyer.dao.EventRepository;
+import pl.bartekbak.lawyer.entity.Event;
+import pl.bartekbak.lawyer.service.EventService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceSpringData implements EventService {
+
+    EventRepository eventRepository;
+
+    @Override
+    public List<Event> findAllCourts() {
+        return eventRepository.findAllByOrderByDateTimeAsc();
+    }
+
+    @Override
+    public Event findEventById(int id) {
+        Optional<Event> result = eventRepository.findById(id);
+        Event event = null;
+        if (result.isPresent()) {
+            event = result.get();
+        } else {
+            throw new RuntimeException("Event id not found");
+        }
+        return event;
+    }
+
+    @Override
+    public void saveEvent(Event event) {
+        eventRepository.save(event);
+    }
+
+    @Override
+    public void deleteEventById(int id) {
+        eventRepository.deleteById(id);
+    }
+}
