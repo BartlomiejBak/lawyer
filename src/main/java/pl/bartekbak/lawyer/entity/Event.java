2092070ac0f8c54d59cfdfbee16e059af38e008b
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-05 09:31:01 +0100

    feat: add list-events, closes #71

--- src/main/java/pl/bartekbak/lawyer/entity/Event.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Event.java
@@ -2,11 +2,20 @@ package pl.bartekbak.lawyer.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
