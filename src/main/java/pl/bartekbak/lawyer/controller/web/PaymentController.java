371d5c0de1b32a1f5d81672d3ad6040621ccbb45
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-16 09:35:00 +0100

    feat: add detail mapping, refactor #88

--- src/main/java/pl/bartekbak/lawyer/controller/web/PaymentController.java
+++ src/main/java/pl/bartekbak/lawyer/controller/web/PaymentController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import pl.bartekbak.lawyer.entity.Payment;
 import pl.bartekbak.lawyer.service.PaymentService;
 
@@ -16,15 +17,16 @@ import java.util.List;
 @RequestMapping("/payments")
 public class PaymentController {
 
-    private PaymentService service;
+    private final PaymentService paymentService;
+    private static final String PAYMENT_ADD_FORM = "payments/add-payment-form";
 
-    public PaymentController(PaymentService service) {
-        this.service = service;
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 
     @GetMapping("/list")
     public String listAllPayments(Model model) {
-        List<Payment> paymentList = service.findAllPayments();
+        List<Payment> paymentList = paymentService.findAllPayments();
         model.addAttribute("payments", paymentList);
         return "payments/list-payments";
     }
@@ -33,31 +35,37 @@ public class PaymentController {
     public String showFormForAdd(Model model) {
         Payment payment = new Payment();
         model.addAttribute("payment", payment);
-        return "payments/add-payment-form";
+        return PAYMENT_ADD_FORM;
     }
 
-    @GetMapping("/showFormForUpdate")
-    public String showFormForUpdate(@RequestParam("paymentId") int id, Model model) {
-        Payment payment = service.findPaymentById(id);
+    @GetMapping("/{paymentId}/edit")
+    public String showFormForUpdate(@PathVariable int paymentId, Model model) {
+        Payment payment = paymentService.findPaymentById(paymentId);
         model.addAttribute(payment);
-        return "payments/add-payment-form";
+        return PAYMENT_ADD_FORM;
     }
 
     @PostMapping("/save")
     public String savePayment(@Valid @ModelAttribute("payment") Payment payment, BindingResult bindingResult) {
         if (bindingResult.hasErrors()){
-            bindingResult.getAllErrors().forEach(objectError -> {
-                log.debug(objectError.toString());
-            });
-            return "payments/add-payment-form";
+            bindingResult.getAllErrors().forEach(objectError ->
+                log.debug(objectError.toString()));
+            return PAYMENT_ADD_FORM;
         }
-        service.savePayment(payment);
+        paymentService.savePayment(payment);
         return "redirect:list";
     }
 
     @GetMapping("/delete")
     public String delete(@RequestParam("paymentId") int id) {
-        service.deletePaymentById(id);
+        paymentService.deletePaymentById(id);
         return "redirect:list";
+    }
+
+    @GetMapping("/{paymentId}")
+    public ModelAndView showPayment(@PathVariable int paymentId) {
+        ModelAndView mav = new ModelAndView("payments/payment-details");
+        mav.addObject(paymentService.findPaymentById(paymentId));
+        return mav;
     }
 }
