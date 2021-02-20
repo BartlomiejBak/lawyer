2048e5e81bc82bfa41c6819b48e3a929a5454c1a
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-20 14:05:00 +0100

    test: add TaskControllerTest, #103

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/TaskControllerTest.java
@@ -0,0 +1,37 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAllTasks() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void saveTask() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void showTask() {
+    }
+}
