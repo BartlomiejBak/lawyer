f15e6659af3c678af98693bb278885735b3c462d
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-20 14:04:14 +0100

    test: add PoaControllerTest, #101

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/PoaControllerTest.java
@@ -0,0 +1,37 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PoaControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAllPoa() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void savePoa() {
+    }
+
+    @Test
+    void deletePoa() {
+    }
+
+    @Test
+    void showPoa() {
+    }
+}
