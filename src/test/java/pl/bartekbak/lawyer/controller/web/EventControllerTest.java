12e25008900bb26c43ffee78eb731e8cc08ad8d5
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-20 14:02:12 +0100

    test: add EventControllerTest, #96

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/EventControllerTest.java
@@ -0,0 +1,37 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAllEvents() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void saveEvent() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void showEvent() {
+    }
+}
