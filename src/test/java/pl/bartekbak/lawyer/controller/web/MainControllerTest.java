5e430263060ef1d6fd2c298611baaafe16d7cbbe
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-20 14:03:04 +0100

    test: add MainControllerTest, #98

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/MainControllerTest.java
@@ -0,0 +1,17 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void showMenu() {
+    }
+}
