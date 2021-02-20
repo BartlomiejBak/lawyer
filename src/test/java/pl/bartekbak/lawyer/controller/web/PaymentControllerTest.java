c95d5b72a13dc6ee6436d4ec977783c427ebca9a
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-20 14:03:49 +0100

    test: add PaymentControllerTest, #100

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/PaymentControllerTest.java
@@ -0,0 +1,37 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAllPayments() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void savePayment() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void showPayment() {
+    }
+}
