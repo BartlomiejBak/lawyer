95959b3d6094e2a42f9125ac13f76870963e7fc2
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-20 14:02:40 +0100

    test: add LawsuitControllerTest, #97

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/LawsuitControllerTest.java
@@ -0,0 +1,37 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LawsuitControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAllLawsuits() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void saveLawsuit() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void showLawsuit() {
+    }
+}
