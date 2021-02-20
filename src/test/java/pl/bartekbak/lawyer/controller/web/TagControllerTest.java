4d8d25c5c0866a7e3b11298a273cc86a13b46539
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-20 14:04:33 +0100

    test: add TagControllerTest, #102

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/TagControllerTest.java
@@ -0,0 +1,33 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAllTags() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void saveTag() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
