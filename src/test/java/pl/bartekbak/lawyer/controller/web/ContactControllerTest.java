38b1d139038a2f9d8d11debabce8e9b23c40d630
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-21 10:14:38 +0100

    test: add ContactControllerTest, #105

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/ContactControllerTest.java
@@ -0,0 +1,37 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAllContacts() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void saveContact() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void showContact() {
+    }
+}
