f78ede55b6ff6f9fcdc441dd2eaa28b4613e636e
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-21 10:15:21 +0100

    test: add AddressControllerTest, #104

---
+++ src/test/java/pl/bartekbak/lawyer/controller/web/AddressControllerTest.java
@@ -0,0 +1,37 @@
+package pl.bartekbak.lawyer.controller.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAddresses() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void saveAddress() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void showAddress() {
+    }
+}
