a562abc10ae92198c5cceab181a8d82b497a4106
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-23 18:59:41 +0100

    test: add MainControllerTest, closes #98

--- src/test/java/pl/bartekbak/lawyer/controller/web/MainControllerTest.java
+++ src/test/java/pl/bartekbak/lawyer/controller/web/MainControllerTest.java
@@ -2,16 +2,35 @@ package pl.bartekbak.lawyer.controller.web;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@ExtendWith(MockitoExtension.class)
 class MainControllerTest {
+
+    @Mock
+    MainController controller;
+
+    MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
-    void showMenu() {
+    void showMenu() throws Exception {
+        mockMvc.perform(get("/main/menu"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("main/menu"));
     }
 }
