a513d126f979ec846e3b21ba3178a4f3f16990a4
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-24 16:31:34 +0100

    refactor: update id naming

--- src/main/java/pl/bartekbak/lawyer/dao/AddressRepository.java
+++ src/main/java/pl/bartekbak/lawyer/dao/AddressRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 
-    public List<Address> findAllByOrderByIdAsc();
+    public List<Address> findAllByOrderByAddressIdAsc();
 }
