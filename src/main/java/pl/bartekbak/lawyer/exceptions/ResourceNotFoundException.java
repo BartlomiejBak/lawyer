6bd9a7a38cbb18003d1356c93e34f7765c37886d
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-03-06 11:46:10 +0100

    feat: update exception

--- src/main/java/pl/bartekbak/lawyer/exceptions/ResourceNotFoundException.java
+++ src/main/java/pl/bartekbak/lawyer/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package pl.bartekbak.lawyer.exceptions;
 
-public class ResourceNotFoundException extends Exception {
+public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
