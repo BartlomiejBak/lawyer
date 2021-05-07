193b580b30f64e58883a39b4ca9c5543e477cd7e
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-05-07 21:15:36 +0200

    feature(api): refactor AddressRestController and add openAPI

--- src/main/java/pl/bartekbak/lawyer/controller/rest/AddressRestController.java
+++ src/main/java/pl/bartekbak/lawyer/controller/rest/AddressRestController.java
@@ -1,16 +1,22 @@
 package pl.bartekbak.lawyer.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.bartekbak.lawyer.dto.AddressDTO;
-import pl.bartekbak.lawyer.entity.Address;
 import pl.bartekbak.lawyer.exceptions.ResourceNotFoundException;
 import pl.bartekbak.lawyer.service.spring.data.AddressServiceSpringData;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/address")
+@RequestMapping("/api/addresses")
 public class AddressRestController {
 
     AddressServiceSpringData addressService;
@@ -20,34 +26,34 @@ public class AddressRestController {
         this.addressService = addressService;
     }
 
-    @GetMapping("/all")
+    @GetMapping
     public List<AddressDTO> getAllAddresses() {
         return addressService.findAllAddresses();
     }
 
-    @GetMapping("/id/{addressId}")
+    @GetMapping("/{addressId}")
     public AddressDTO getAddress(@PathVariable int addressId) {
-        AddressDTO agreement = addressService.findAddressById(addressId);
-        if (agreement == null) {
+        AddressDTO address = addressService.findAddressById(addressId);
+        if (address == null) {
             throw new ResourceNotFoundException("No such Id in database");
         }
-        return agreement;
+        return address;
     }
 
-    @PostMapping("/register")
+    @PostMapping
     public AddressDTO addAddress(@RequestBody AddressDTO address) {
         address.setAddressId(0);
         addressService.saveAddress(address);
         return address;
     }
 
-    @PutMapping("/register")
+    @PutMapping
     public AddressDTO updateAddress(@RequestBody AddressDTO address) {
         addressService.saveAddress(address);
         return address;
     }
 
-    @DeleteMapping("/remove/{addressId}")
+    @DeleteMapping("/{addressId}")
     public String deleteAddress(@PathVariable int addressId) {
         AddressDTO address = addressService.findAddressById(addressId);
         if (address == null) {
