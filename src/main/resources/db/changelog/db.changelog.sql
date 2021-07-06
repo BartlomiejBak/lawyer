8b61d76ec8e84a8b1ca4eab2c4dfd3a981ed245c
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-07-06 20:54:28 +0200

    feature(db): add address table

--- src/main/resources/db/changelog/db.changelog.sql
+++ src/main/resources/db/changelog/db.changelog.sql
@@ -1,1 +1,13 @@
 --liquibase formatted sql
+
+--changeset bbak:1
+
+CREATE SEQUENCE table_id_seq;
+
+CREATE TABLE IF NOT EXISTS ADDRESS (
+                         address_id int PRIMARY KEY NOT NULL DEFAULT nextval('table_id_seq'),
+                         street VARCHAR(50),
+                         zip_code VARCHAR(10),
+                         city VARCHAR(50),
+                         country VARCHAR(20)
+);
