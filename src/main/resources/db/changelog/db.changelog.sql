e0a589db4fa8b36db18d9cc00590d01acf7371e6
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-07-07 20:10:57 +0200

    feature(db): add lawsuit table

--- src/main/resources/db/changelog/db.changelog.sql
+++ src/main/resources/db/changelog/db.changelog.sql
@@ -11,3 +11,17 @@ CREATE TABLE IF NOT EXISTS ADDRESS (
                          city VARCHAR(50),
                          country VARCHAR(20)
 );
+
+--changeset bbak:2
+
+CREATE TABLE LAWSUIT (
+                         case_id INT PRIMARY KEY NOT NULL DEFAULT nextval('table_id_seq'),
+                         name VARCHAR(50),
+                         cas_side VARCHAR(50),
+                         input_date DATE,
+                         deadline DATE,
+                         signature VARCHAR(20),
+                         claim_amount DECIMAL(12,2),
+                         add_info VARCHAR(500)
+);
+
