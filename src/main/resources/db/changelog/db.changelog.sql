31e8c7b9877658457d1486c8f0ebab7e59531b36
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-07-08 15:56:57 +0200

    feature(db): add contact table

--- src/main/resources/db/changelog/db.changelog.sql
+++ src/main/resources/db/changelog/db.changelog.sql
@@ -25,3 +25,24 @@ CREATE TABLE LAWSUIT (
                          add_info VARCHAR(500)
 );
 
+--changeset bbak:3
+
+CREATE TABLE CONTACT (
+                         contact_id INT PRIMARY KEY NOT NULL DEFAULT nextval('table_id_seq'),
+                         name VARCHAR(45),
+                         first_name VARCHAR(45),
+                         last_name VARCHAR(45),
+                         email VARCHAR(45),
+                         alt_email VARCHAR(45),
+                         phone VARCHAR(20),
+                         alt_phone VARCHAR(20),
+                         pesel VARCHAR(15),
+                         address INT REFERENCES ADDRESS(address_id),
+                         company_name VARCHAR(45),
+                         nip VARCHAR(20),
+                         regon VARCHAR(20),
+                         krs VARCHAR(20),
+                         date_created DATE,
+                         modified TIMESTAMP
+);
+
