e6aec09a159d850bcc5a3b834200f63f62413c33
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-08-26 21:17:31 +0200

    refactor(jooq): update contact entity and repository interface

--- src/main/java/pl/bartekbak/lawyer/entity/Contact.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Contact.java
@@ -1,77 +1,51 @@
 package pl.bartekbak.lawyer.entity;
 
 import lombok.*;
-import org.hibernate.Hibernate;
 import pl.bartekbak.lawyer.dto.ContactDTO;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-@Entity
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "contact")
 public class Contact {
 
-    @Id
-    @Column(name = "contact_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int contactId;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
     private String email;
 
-    @Column(name = "alt_email")
     private String altEmail;
 
-    @Column(name = "phone")
     private String phone;
 
-    @Column(name = "alt_phone")
     private String altPhone;
 
-    @Column(name = "company_name")
     private String companyName;
 
-    @Column(name = "pesel")
     private String pesel;
 
-    @Column(name = "nip")
     private String nip;
 
-    @Column(name = "regon")
     private String regon;
 
-    @Column(name = "krs")
     private String krs;
 
-    @Column(name = "date_created")
     private LocalDateTime dateCreated;
 
-    @Column(name = "modified")
     private LocalDateTime dateModified;
 
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-    @JoinColumn(name = "address", referencedColumnName = "address_id")
     @ToString.Exclude
     private Address address;
 
-    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-    @JoinColumn(name = "secondary_address", referencedColumnName = "address_id")
     @ToString.Exclude
     private Address correspondenceAddress;
 
@@ -120,16 +94,4 @@ public class Contact {
                 .build();
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Contact contact = (Contact) o;
-        return Objects.equals(contactId, contact.contactId);
-    }
-
-    @Override
-    public int hashCode() {
-        return 0;
-    }
 }
---
+++ src/main/java/pl/bartekbak/lawyer/repository/ContactRepository.java
@@ -0,0 +1,14 @@
+package pl.bartekbak.lawyer.repository;
+
+import pl.bartekbak.lawyer.entity.Contact;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ContactRepository {
+
+    List<Contact> list();
+    Optional<Contact> contactById(int id);
+    void add(Contact contact);
+    void deleteById(int id);
+}
