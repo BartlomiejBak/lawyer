875cf3f7e5dadfa5272156d9fcbe7e6a4f64afc1
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-08-28 19:39:55 +0200

    refactor(jooq): update court entity and repository interface

---
+++ src/main/java/pl/bartekbak/lawyer/repository/CourtRepository.java
@@ -0,0 +1,14 @@
+package pl.bartekbak.lawyer.repository;
+
+import pl.bartekbak.lawyer.entity.Court;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CourtRepository {
+
+    List<Court> list();
+    Optional<Court> courtById(int id);
+    void add(Court court);
+    void deleteById(int id);
+}
