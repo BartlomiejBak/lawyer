e7dfe6f8222418d9f10ed992f87052744669e552
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-09-03 18:11:42 +0200

    refactor(jooq): update note entity and repository interface

--- src/main/java/pl/bartekbak/lawyer/entity/Note.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Note.java
@@ -6,25 +6,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import pl.bartekbak.lawyer.dto.NoteDTO;
 
-import javax.persistence.*;
-
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "note")
 public class Note {
 
-    @Id
-    @Column(name = "note_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int noteId;
 
-    @Column(name = "title")
     private String title;
 
-    @Column(name = "text")
     private String text;
 
     public NoteDTO toDto() {
---
+++ src/main/java/pl/bartekbak/lawyer/repository/NoteRepository.java
@@ -0,0 +1,14 @@
+package pl.bartekbak.lawyer.repository;
+
+import pl.bartekbak.lawyer.entity.Note;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface NoteRepository {
+
+    List<Note> list();
+    Optional<Note> noteById(int id);
+    void add(Note note);
+    void deleteById(int id);
+}
