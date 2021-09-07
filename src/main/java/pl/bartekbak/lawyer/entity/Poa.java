a5cdfab62758257a71274006f564563c24280b1f
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-09-07 22:33:21 +0200

    refactor(jooq): update poa entity and repository interface

--- src/main/java/pl/bartekbak/lawyer/entity/Poa.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Poa.java
@@ -6,49 +6,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import pl.bartekbak.lawyer.dto.PoaDTO;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.time.LocalDate;
 
-@Entity
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "poa")
 public class Poa {
 
-    @Id
-    @Column(name = "poa_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int poaId;
 
-    @Column(name = "type")
     private String type;
 
-    @Column(name = "payment")
     private String payment;
 
-    @Column(name = "kpc")
     private boolean kpc;
 
-    @Column(name = "termination")
     private boolean termination;
 
-    @Column(name = "start_date")
     private LocalDate startDate;
 
-    @Column(name = "end_date")
     private LocalDate endDate;
 
-    @Column(name = "notification_duty")
     private boolean terminationNotificationDuty;
 
-    @Column(name = "duty_completed")
     private boolean terminationNotificationDutyCompleted;
 
     public PoaDTO toDto() {
---
+++ src/main/java/pl/bartekbak/lawyer/repository/PoaRepository.java
@@ -0,0 +1,14 @@
+package pl.bartekbak.lawyer.repository;
+
+import pl.bartekbak.lawyer.entity.Poa;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PoaRepository {
+
+    List<Poa> list();
+    Optional<Poa> poaById(int id);
+    void add(Poa poa);
+    void deleteById(int id);
+}
