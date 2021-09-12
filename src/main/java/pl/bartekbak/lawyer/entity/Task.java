e11c3797e604bf777e369f60ad3cffe656947e59
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-09-12 20:41:25 +0200

    refactor(jooq): update task entity and repository interface

--- src/main/java/pl/bartekbak/lawyer/entity/Task.java
+++ src/main/java/pl/bartekbak/lawyer/entity/Task.java
@@ -6,40 +6,25 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import pl.bartekbak.lawyer.dto.TaskDTO;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "task")
 public class Task {
 
-    @Id
-    @Column(name = "task_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int taskId;
 
-    @Column(name = "priority")
     private boolean priority;
 
-    @Column(name = "deadline")
     private LocalDateTime deadline;
 
-    @Column(name = "description")
     private String description;
 
-    @ManyToMany(cascade = CascadeType.PERSIST)
-    @JoinTable(
-            name = "task_contact",
-            joinColumns = @JoinColumn(name = "task"),
-            inverseJoinColumns = @JoinColumn(name = "contact")
-    )
     @Builder.Default
     private List<Contact> contactList = new ArrayList<>();
 
---
+++ src/main/java/pl/bartekbak/lawyer/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package pl.bartekbak.lawyer.repository;
+
+import pl.bartekbak.lawyer.entity.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+
+    List<Task> list();
+    Optional<Task> taskById(int id);
+    void add(Task task);
+    void deleteById(int id);
+}
