c074cfe94f950564aa39e09fcfb40241531fd37e
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-09-25 20:30:12 +0200

    feature(jooq): implement jooq in address repository

--- src/main/java/pl/bartekbak/lawyer/repository/AddressRepository.java
+++ src/main/java/pl/bartekbak/lawyer/repository/AddressRepository.java
@@ -10,6 +10,7 @@ public interface AddressRepository {
     List<Address> list();
     Optional<Address> addressById(int id);
     void add(Address address);
+    void update(Address address);
     void deleteById(int id);
 
 }
--- src/main/java/pl/bartekbak/lawyer/repository/jooq/AddressRepositoryImpl.java
+++ src/main/java/pl/bartekbak/lawyer/repository/jooq/AddressRepositoryImpl.java
@@ -1,31 +1,68 @@
 package pl.bartekbak.lawyer.repository.jooq;
 
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pl.bartekbak.lawyer.common.DatabaseContext;
 import pl.bartekbak.lawyer.entity.Address;
 import pl.bartekbak.lawyer.repository.AddressRepository;
 
 import java.util.List;
 import java.util.Optional;
 
+import static pl.bartekbak.lawyer.generate.jooq.tables.DbAddress.DB_ADDRESS;
+
 @Repository
-public class AddressRepositoryImpl implements AddressRepository {
+public class AddressRepositoryImpl extends DatabaseContext implements AddressRepository {
+
+    public AddressRepositoryImpl(DSLContext dslContext) {
+        super(dslContext);
+    }
+
     @Override
     public List<Address> list() {
-        return List.of();
+        return dslContext().selectFrom(DB_ADDRESS)
+                .fetch()
+                .map(Address::fromDbRecord);
     }
 
     @Override
     public Optional<Address> addressById(int id) {
-        return Optional.empty();
+        return dslContext().selectFrom(DB_ADDRESS)
+                .where(DB_ADDRESS.ADDRESS_ID.eq(id))
+                .fetchOptional()
+                .map(Address::fromDbRecord);
     }
 
     @Override
+    @Transactional
     public void add(Address address) {
-
+        dslContext().insertInto(DB_ADDRESS)
+                .set(DB_ADDRESS.CITY, address.getCity())
+                .set(DB_ADDRESS.COUNTRY, address.getCountry())
+                .set(DB_ADDRESS.STREET, address.getStreet())
+                .set(DB_ADDRESS.ZIP_CODE, address.getZipCode())
+                .onDuplicateKeyIgnore()
+                .execute();
     }
 
     @Override
-    public void deleteById(int id) {
+    @Transactional
+    public void update(Address address) {
+        dslContext().update(DB_ADDRESS)
+                .set(DB_ADDRESS.CITY, address.getCity())
+                .set(DB_ADDRESS.COUNTRY, address.getCountry())
+                .set(DB_ADDRESS.STREET, address.getStreet())
+                .set(DB_ADDRESS.ZIP_CODE, address.getZipCode())
+                .where(DB_ADDRESS.ADDRESS_ID.eq(address.getAddressId()))
+                .execute();
+    }
 
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        dslContext().deleteFrom(DB_ADDRESS)
+                .where(DB_ADDRESS.ADDRESS_ID.eq(id))
+                .execute();
     }
 }
