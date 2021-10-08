0c1b977ca32903f6c523c97874926c96a333ec0f
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-10-08 22:44:43 +0200

    feature(jooq): implement jooq in contact repository

--- src/main/java/pl/bartekbak/lawyer/repository/jooq/ContactRepositoryImpl.java
+++ src/main/java/pl/bartekbak/lawyer/repository/jooq/ContactRepositoryImpl.java
@@ -2,6 +2,7 @@ package pl.bartekbak.lawyer.repository.jooq;
 
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pl.bartekbak.lawyer.common.DatabaseContext;
 import pl.bartekbak.lawyer.entity.Contact;
 import pl.bartekbak.lawyer.generate.jooq.tables.DbAddress;
@@ -51,21 +52,84 @@ public class ContactRepositoryImpl extends DatabaseContext implements ContactRep
 
     @Override
     public Optional<Contact> contactById(int id) {
-        return Optional.empty();
+        return Optional.ofNullable(dslContext().select(
+                        DB_CONTACT.asterisk(),
+                        field(
+                                select(jsonObject(DB_ADDRESS.fields()))
+                                        .from(DB_ADDRESS)
+                                        .join(DB_CONTACT_ADDRESS)
+                                        .on(DB_ADDRESS.ADDRESS_ID.eq(DB_CONTACT_ADDRESS.ADDRESS))
+                                        .where(DB_CONTACT_ADDRESS.CONTACT.eq(DB_CONTACT.CONTACT_ID))
+                        )
+                                .as("address"),
+                        field(
+                                select(jsonObject(secondary.fields()))
+                                        .from(secondary)
+                                        .join(DB_CONTACT_ADDRESS)
+                                        .on(secondary.ADDRESS_ID.eq(DB_CONTACT_ADDRESS.ADDRESS))
+                                        .where(DB_CONTACT_ADDRESS.CONTACT.eq(DB_CONTACT.CONTACT_ID))
+                        )
+                                .as("correspondenceAddress")
+                )
+                .from(DB_CONTACT)
+                .where(DB_CONTACT.CONTACT_ID.eq(id))
+                .fetchOneInto(Contact.class));
     }
 
     @Override
+    @Transactional
     public void add(Contact contact) {
-
+        dslContext().insertInto(DB_CONTACT)
+                .set(DB_CONTACT.NAME, contact.getName())
+                .set(DB_CONTACT.FIRST_NAME, contact.getFirstName())
+                .set(DB_CONTACT.LAST_NAME, contact.getLastName())
+                .set(DB_CONTACT.EMAIL, contact.getEmail())
+                .set(DB_CONTACT.ALT_EMAIL, contact.getAltEmail())
+                .set(DB_CONTACT.PHONE, contact.getPhone())
+                .set(DB_CONTACT.ALT_PHONE, contact.getAltPhone())
+                .set(DB_CONTACT.COMPANY_NAME, contact.getCompanyName())
+                .set(DB_CONTACT.PESEL, contact.getPesel())
+                .set(DB_CONTACT.NIP, contact.getNip())
+                .set(DB_CONTACT.REGON, contact.getRegon())
+                .set(DB_CONTACT.KRS, contact.getKrs())
+                .set(DB_CONTACT.DATE_CREATED, contact.getDateCreated())
+                .set(DB_CONTACT.MODIFIED, contact.getDateModified())
+                .onDuplicateKeyIgnore()
+                .execute();
     }
 
+    // todo add/remove address to contact
+
     @Override
+    @Transactional
     public void update(Contact contact) {
-
+        dslContext().update(DB_CONTACT)
+                .set(DB_CONTACT.NAME, contact.getName())
+                .set(DB_CONTACT.FIRST_NAME, contact.getFirstName())
+                .set(DB_CONTACT.LAST_NAME, contact.getLastName())
+                .set(DB_CONTACT.EMAIL, contact.getEmail())
+                .set(DB_CONTACT.ALT_EMAIL, contact.getAltEmail())
+                .set(DB_CONTACT.PHONE, contact.getPhone())
+                .set(DB_CONTACT.ALT_PHONE, contact.getAltPhone())
+                .set(DB_CONTACT.COMPANY_NAME, contact.getCompanyName())
+                .set(DB_CONTACT.PESEL, contact.getPesel())
+                .set(DB_CONTACT.NIP, contact.getNip())
+                .set(DB_CONTACT.REGON, contact.getRegon())
+                .set(DB_CONTACT.KRS, contact.getKrs())
+                .set(DB_CONTACT.DATE_CREATED, contact.getDateCreated())
+                .set(DB_CONTACT.MODIFIED, contact.getDateModified())
+                .where(DB_CONTACT.CONTACT_ID.eq(contact.getContactId()))
+                .execute();
     }
 
     @Override
+    @Transactional
     public void deleteById(int id) {
-
+        dslContext().deleteFrom(DB_CONTACT_ADDRESS)
+                .where(DB_CONTACT_ADDRESS.CONTACT.eq(id))
+                .execute();
+        dslContext().deleteFrom(DB_CONTACT)
+                .where(DB_CONTACT.CONTACT_ID.eq(id))
+                .execute();
     }
 }
