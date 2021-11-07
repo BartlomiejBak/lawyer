2a4aeb8fc5441a3f9b565c22958afda59f7b9533
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-11-07 15:58:20 +0100

    test(tag): add data to provider

--- src/test/java/pl/bartekbak/lawyer/repository/DataProvider.java
+++ src/test/java/pl/bartekbak/lawyer/repository/DataProvider.java
@@ -2,8 +2,11 @@ package pl.bartekbak.lawyer.repository;
 
 import com.github.javafaker.Faker;
 import org.jooq.DSLContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Locale;
+
+import static pl.bartekbak.lawyer.generate.jooq.Tables.DB_TAG;
 
 public class DataProvider {
 
@@ -14,7 +17,26 @@ public class DataProvider {
         this.context = context;
     }
 
-    void initStandardScenario() {
+    public void initStandardScenario() {
+        addRandomTag(1);
+        addRandomTag(2);
+        addRandomTag(3);
+        addRandomTag(4);
+        addRandomTag(5);
+        addRandomTag(6);
+    }
 
+    @Transactional
+    public void addRandomTag(int id) {
+        context.insertInto(DB_TAG)
+                .set(DB_TAG.TAG_ID, id)
+                .set(DB_TAG.NAME, faker.dog().name())
+                .onDuplicateKeyIgnore()
+                .execute();
+    }
+
+    @Transactional
+    public void clearDatabase() {
+        context.deleteFrom(DB_TAG).execute();
     }
 }
