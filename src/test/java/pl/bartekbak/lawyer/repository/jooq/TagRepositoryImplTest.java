55968b2e082ad53a1fe5b2e8d4bc8c36a9ad0591
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-11-10 21:29:09 +0100

    test(tag): test add tag

--- src/test/java/pl/bartekbak/lawyer/repository/jooq/TagRepositoryImplTest.java
+++ src/test/java/pl/bartekbak/lawyer/repository/jooq/TagRepositoryImplTest.java
@@ -1,5 +1,6 @@
 package pl.bartekbak.lawyer.repository.jooq;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.bartekbak.lawyer.common.PostgreSQLJooqContainer;
+import pl.bartekbak.lawyer.entity.Tag;
 import pl.bartekbak.lawyer.repository.DataProvider;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +19,7 @@ class TagRepositoryImplTest {
 
     @Container
     public static PostgreSQLJooqContainer container = new PostgreSQLJooqContainer();
+    private Faker faker = Faker.instance();
 
     static DataProvider provider;
     static TagRepositoryImpl repository;
@@ -43,6 +46,7 @@ class TagRepositoryImplTest {
 
         // when
         final var result = repository.list();
+
         // then
         assertThat(result.size()).isEqualTo(6);
     }
@@ -51,8 +55,10 @@ class TagRepositoryImplTest {
     void should_return_empty_list_when_database_cleared() {
         // given
         provider.clearDatabase();
+
         // when
         final var result = repository.list();
+
         // then
         assertThat(result).isEmpty();
     }
@@ -61,8 +67,10 @@ class TagRepositoryImplTest {
     void should_return_optional_of_existing_tag() {
         // given
         int tagId = 1;
+
         // when
         final var result = repository.tagById(tagId);
+
         // then
         assertThat(result).isPresent();
         assertThat(result.get().getTagId()).isEqualTo(tagId);
@@ -72,9 +80,46 @@ class TagRepositoryImplTest {
     void should_return_empty_optional() {
         // given
         int tagId = Integer.MAX_VALUE;
+
         // when
         final var result = repository.tagById(tagId);
+
         // then
         assertThat(result).isEmpty();
     }
+
+    @Test
+    void should_add_tag_when_not_exists() {
+        // given
+        int givenId = Integer.MAX_VALUE;
+        String name = faker.dog().name();
+        Tag givenTag = Tag.builder().tagId(givenId).name(name).build();
+
+        // when
+        repository.add(givenTag);
+        var result = repository.tagById(givenId);
+
+        // then
+        assertThat(result).isPresent();
+        assertThat(result.get().getName()).isEqualTo(name);
+    }
+
+    @Test
+    void should_do_nothing_when_adding_non_unique_value() {
+        // given
+        int givenId = Integer.MAX_VALUE;
+        int additionalId = Integer.MAX_VALUE - 1;
+        String name = faker.dog().name();
+        Tag givenTag = Tag.builder().tagId(givenId).name(name).build();
+        Tag duplicateTag = Tag.builder().tagId(additionalId).name(name).build();
+
+        // when
+        repository.add(givenTag);
+        repository.add(duplicateTag);
+        var result = repository.tagById(additionalId);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
 }
