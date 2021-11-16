b3d55c043d264e27551167231440d58f5c401f03
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-11-16 22:19:43 +0100

    test(note): add note to provider

--- src/test/java/pl/bartekbak/lawyer/repository/DataProvider.java
+++ src/test/java/pl/bartekbak/lawyer/repository/DataProvider.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Locale;
 
+import static pl.bartekbak.lawyer.generate.jooq.Tables.DB_NOTE;
 import static pl.bartekbak.lawyer.generate.jooq.Tables.DB_TAG;
 
 public class DataProvider {
@@ -24,6 +25,13 @@ public class DataProvider {
         addRandomTag(4);
         addRandomTag(5);
         addRandomTag(6);
+
+        addRandomNote(7);
+        addRandomNote(8);
+        addRandomNote(9);
+        addRandomNote(10);
+        addRandomNote(11);
+        addRandomNote(12);
     }
 
     @Transactional
@@ -36,7 +44,18 @@ public class DataProvider {
     }
 
     @Transactional
+    public void addRandomNote(int id) {
+        context.insertInto(DB_NOTE)
+                .set(DB_NOTE.NOTE_ID, id)
+                .set(DB_NOTE.TITLE, faker.book().title())
+                .set(DB_NOTE.TEXT, faker.lorem().paragraph())
+                .onDuplicateKeyIgnore()
+                .execute();
+    }
+
+    @Transactional
     public void clearDatabase() {
         context.deleteFrom(DB_TAG).execute();
+        context.deleteFrom(DB_NOTE).execute();
     }
 }
