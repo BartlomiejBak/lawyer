fcbb78913267170b9c7389f8cf090b6aeaebbdcb
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-11-19 19:35:19 +0100

    test(note): test add note

--- src/test/java/pl/bartekbak/lawyer/repository/jooq/NoteRepositoryImplTest.java
+++ src/test/java/pl/bartekbak/lawyer/repository/jooq/NoteRepositoryImplTest.java
@@ -1,6 +1,7 @@
 package pl.bartekbak.lawyer.repository.jooq;
 
 import com.github.javafaker.Faker;
+import org.apache.logging.log4j.util.Strings;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.bartekbak.lawyer.common.PostgreSQLJooqContainer;
+import pl.bartekbak.lawyer.entity.Note;
 import pl.bartekbak.lawyer.repository.DataProvider;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -82,6 +84,44 @@ class NoteRepositoryImplTest {
 
         // when
         final var result = repository.noteById(noteId);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void should_add_note_when_not_exists() {
+        // given
+        int givenId = Integer.MAX_VALUE;
+        String title = faker.book().title();
+        String text = faker.lorem().paragraph();
+        var givenNote = Note.builder().noteId(givenId)
+                .title(title)
+                .text(text)
+                .build();
+
+        // when
+        repository.add(givenNote);
+        var result = repository.noteById(givenId);
+
+        // then
+        assertThat(result).isPresent();
+        assertThat(result.get().getTitle()).isEqualTo(title);
+    }
+
+    @Test
+    void should_do_nothing_when_adding_non_unique_value() {
+        // given
+        int givenId = Integer.MAX_VALUE;
+        int additionalId = Integer.MAX_VALUE - 1;
+        String title = faker.dog().name();
+        var givenNote = Note.builder().noteId(givenId).title(title).text(Strings.EMPTY).build();
+        var duplicateNote = Note.builder().noteId(additionalId).title(title).text(Strings.EMPTY).build();
+
+        // when
+        repository.add(givenNote);
+        repository.add(duplicateNote);
+        var result = repository.noteById(additionalId);
 
         // then
         assertThat(result).isEmpty();
