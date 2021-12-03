3b4c65bb2fe43204baf576bb27c9ecc0cd3e6b8c
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-12-03 19:02:00 +0100

    test(payment): add data to provider

--- src/test/java/pl/bartekbak/lawyer/repository/DataProvider.java
+++ src/test/java/pl/bartekbak/lawyer/repository/DataProvider.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.util.Locale;
 
 import static pl.bartekbak.lawyer.generate.jooq.Tables.DB_NOTE;
+import static pl.bartekbak.lawyer.generate.jooq.Tables.DB_PAYMENT;
 import static pl.bartekbak.lawyer.generate.jooq.Tables.DB_POA;
 import static pl.bartekbak.lawyer.generate.jooq.Tables.DB_TAG;
 
@@ -19,6 +20,7 @@ public class DataProvider {
     public static int TAG_ID = 1;
     public static int NOTE_ID = 7;
     public static int POA_ID = 13;
+    public static int PAYMENT_ID = 19;
 
     public DataProvider(DSLContext context) {
         this.context = context;
@@ -45,6 +47,13 @@ public class DataProvider {
         addRandomPoa(16);
         addRandomPoa(17);
         addRandomPoa(18);
+
+        addRandomPayment(PAYMENT_ID);
+        addRandomPayment(20);
+        addRandomPayment(21);
+        addRandomPayment(22);
+        addRandomPayment(23);
+        addRandomPayment(24);
     }
 
     @Transactional
@@ -79,6 +88,22 @@ public class DataProvider {
                 .set(DB_POA.END_DATE, date.plusDays(faker.number().randomDigitNotZero()))
                 .set(DB_POA.NOTIFICATION_DUTY, true)
                 .set(DB_POA.NOTIFICATION_DUTY, faker.bool().bool())
+                .onDuplicateKeyIgnore()
+                .execute();
+    }
+
+    @Transactional
+    public void addRandomPayment(int id) {
+        var date = LocalDate.from(faker.date().birthday().toInstant());
+        context.insertInto(DB_PAYMENT)
+                .set(DB_PAYMENT.PAYMENT_ID, id)
+                .set(DB_PAYMENT.PAYMENT_VALUE, faker.number().randomDouble(2, 1, Integer.MAX_VALUE))
+                .set(DB_PAYMENT.PAYMENT_DATE, date)
+                .set(DB_PAYMENT.PAID, true)
+                .set(DB_PAYMENT.PAID_DATE, date.plusDays(30))
+                .set(DB_PAYMENT.COMMENT, faker.lordOfTheRings().location())
+                .set(DB_PAYMENT.US, faker.bool().bool())
+                .set(DB_PAYMENT.INCOMING, faker.bool().bool())
                 .onDuplicateKeyIgnore()
                 .execute();
     }
