81c9e78f8d9d12f6f28103d39fbe1e35d75adf51
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-12-16 22:18:23 +0100

    test(address): test delete address

--- src/test/java/pl/bartekbak/lawyer/repository/jooq/AddressRepositoryImplTest.java
+++ src/test/java/pl/bartekbak/lawyer/repository/jooq/AddressRepositoryImplTest.java
@@ -210,4 +210,32 @@ class AddressRepositoryImplTest {
         assertThat(result).isEmpty();
     }
 
+    @Test
+    void should_delete_record_when_exists() {
+        // given
+        int givenId = DataProvider.ADDRESS_ID;
+        var beforeDelete = repository.addressById(givenId);
+
+        // when
+        repository.deleteById(givenId);
+        var afterDelete = repository.addressById(givenId);
+
+        // then
+        assertThat(beforeDelete).isPresent();
+        assertThat(afterDelete).isEmpty();
+    }
+
+    @Test
+    void should_do_nothing_when_delete_nonexistent_record() {
+        // given
+        int givenId = Integer.MAX_VALUE;
+        var beforeDelete = repository.list();
+
+        // when
+        repository.deleteById(givenId);
+        var afterDelete = repository.list();
+
+        // then
+        assertThat(beforeDelete.size()).isEqualTo(afterDelete.size());
+    }
 }
