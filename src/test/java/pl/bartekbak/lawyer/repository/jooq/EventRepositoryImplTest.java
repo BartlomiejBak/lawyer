11b705a0cc9027823e42bd13bd3d1e2c0faf1cb3
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-12-19 19:26:54 +0100

    test(event): test add event

--- src/test/java/pl/bartekbak/lawyer/repository/jooq/EventRepositoryImplTest.java
+++ src/test/java/pl/bartekbak/lawyer/repository/jooq/EventRepositoryImplTest.java
@@ -8,7 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.bartekbak.lawyer.common.PostgreSQLJooqContainer;
+import pl.bartekbak.lawyer.entity.Event;
 import pl.bartekbak.lawyer.repository.DataProvider;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -82,6 +86,53 @@ class EventRepositoryImplTest {
 
         // when
         final var result = repository.eventById(eventId);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void should_add_event_when_not_exists() {
+        // given
+        int givenId = Integer.MAX_VALUE;
+        var givenEvent = Event.builder()
+                .eventId(givenId)
+                .title(faker.book().title())
+                .description(faker.lorem().paragraph())
+                .dateTime(LocalDateTime.now().plus(100, ChronoUnit.DAYS))
+                .build();
+
+        // when
+        repository.add(givenEvent);
+        var result = repository.eventById(givenId);
+
+        // then
+        assertThat(result).isPresent().contains(givenEvent);
+    }
+
+    @Test
+    void should_do_nothing_when_adding_non_unique_value() {
+        // given
+        int givenId = Integer.MAX_VALUE;
+        int duplicateId = givenId - 1;
+
+        var givenEvent = Event.builder()
+                .eventId(givenId)
+                .title(faker.book().title())
+                .description(faker.lorem().paragraph())
+                .dateTime(LocalDateTime.now().plus(100, ChronoUnit.DAYS))
+                .build();
+        var duplicateEvent = Event.builder()
+                .eventId(givenId)
+                .title(givenEvent.getTitle())
+                .description(givenEvent.getDescription())
+                .dateTime(givenEvent.getDateTime())
+                .build();
+
+        // when
+        repository.add(givenEvent);
+        repository.add(duplicateEvent);
+        var result = repository.eventById(duplicateId);
 
         // then
         assertThat(result).isEmpty();
