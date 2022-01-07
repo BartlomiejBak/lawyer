7697358d5e52337e6891a9fe0e7a7f3196d4b5cf
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-01-07 15:24:36 +0100

    test(lawsuit): test delete lawsuit

--- src/test/java/pl/bartekbak/lawyer/repository/jooq/LawsuitRepositoryImplTest.java
+++ src/test/java/pl/bartekbak/lawyer/repository/jooq/LawsuitRepositoryImplTest.java
@@ -172,4 +172,33 @@ class LawsuitRepositoryImplTest {
         // then
         assertThat(result).isEmpty();
     }
+
+    @Test
+    void should_delete_record_when_exists() {
+        // given
+        var givenId = DataProvider.LAWSUIT_ID;
+        var beforeDelete = repository.lawsuitById(givenId);
+
+        // when
+        repository.deleteById(givenId);
+        var afterDelete = repository.lawsuitById(givenId);
+
+        // then
+        assertThat(beforeDelete).isPresent();
+        assertThat(afterDelete).isEmpty();
+    }
+
+    @Test
+    void should_do_nothing_when_delete_nonexistent_record() {
+        // given
+        var givenId = UUID.randomUUID();
+        var beforeDelete = repository.list();
+
+        // when
+        repository.deleteById(givenId);
+        var afterDelete = repository.list();
+
+        // then
+        assertThat(beforeDelete.size()).isEqualTo(afterDelete.size());
+    }
 }
