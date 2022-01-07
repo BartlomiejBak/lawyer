532bdf5b4e6fcc4d31e870fe8d5bd1d3a4593155
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-01-07 12:25:12 +0100

    test(court): test list courts

---
+++ src/test/java/pl/bartekbak/lawyer/repository/jooq/CourtRepositoryImplTest.java
@@ -0,0 +1,64 @@
+package pl.bartekbak.lawyer.repository.jooq;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.bartekbak.lawyer.common.PostgreSQLJooqContainer;
+import pl.bartekbak.lawyer.repository.DataProvider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+class CourtRepositoryImplTest {
+
+    @Container
+    public static PostgreSQLJooqContainer container = new PostgreSQLJooqContainer();
+    private final Faker faker = Faker.instance();
+
+    static DataProvider provider;
+    static CourtRepositoryImpl repository;
+
+    @BeforeAll
+    static void beforeAll() {
+        provider = new DataProvider(container.dsl());
+        repository = new CourtRepositoryImpl(container.dsl());
+    }
+
+    @BeforeEach
+    void setUp() {
+        provider.initStandardScenario();
+    }
+
+    @AfterEach
+    void tearDown() {
+        provider.clearDatabase();
+    }
+
+    @Test
+    void should_return_initial_list() {
+        // given
+
+        // when
+        final var result = repository.list();
+
+        // then
+        assertThat(result.size()).isEqualTo(6);
+    }
+
+    @Test
+    void should_return_empty_list_when_database_cleared() {
+        // given
+        provider.clearDatabase();
+
+        // when
+        final var result = repository.list();
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+}
