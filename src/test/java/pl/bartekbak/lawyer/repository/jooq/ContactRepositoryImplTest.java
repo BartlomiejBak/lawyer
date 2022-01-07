954671f70869d3caab9fa0dcc34d5fb9ad53613a
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-01-07 15:06:30 +0100

    test(contact): test add and update contact

--- src/test/java/pl/bartekbak/lawyer/repository/jooq/ContactRepositoryImplTest.java
+++ src/test/java/pl/bartekbak/lawyer/repository/jooq/ContactRepositoryImplTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.bartekbak.lawyer.common.PostgreSQLJooqContainer;
+import pl.bartekbak.lawyer.entity.Contact;
 import pl.bartekbak.lawyer.repository.DataProvider;
 
 import java.util.UUID;
@@ -88,5 +89,88 @@ class ContactRepositoryImplTest {
         assertThat(result).isEmpty();
     }
 
+    @Test
+    void should_add_record_when_not_exists() {
+        // given
+        var givenId = UUID.randomUUID();
+        String name = faker.dog().name();
+        var givenRecord = Contact.builder().contactId(givenId).name(name).build();
+
+        // when
+        repository.add(givenRecord);
+        var result = repository.contactById(givenId);
+
+        // then
+        assertThat(result).isPresent();
+        assertThat(result.get().getName()).isEqualTo(name);
+    }
+
+    @Test
+    void should_do_nothing_when_adding_non_unique_value() {
+        // given
+        var givenId = UUID.randomUUID();
+        var additionalId = UUID.randomUUID();
+        String name = faker.dog().name();
+        var givenRecord = Contact.builder().contactId(givenId).name(name).build();
+        var duplicateRecord = Contact.builder().contactId(additionalId).name(name).build();
+
+        // when
+        repository.add(givenRecord);
+        repository.add(duplicateRecord);
+        var result = repository.contactById(additionalId);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void should_update_value_when_record_exists() {
+        // given
+        var givenId = DataProvider.CONTACT_ID;
+        String name = faker.cat().name();
+        var givenRecord = Contact.builder().contactId(givenId).name(name).build();
+
+        // when
+        repository.update(givenRecord);
+        var result = repository.contactById(givenId);
+
+        // then
+        assertThat(result).isNotEmpty();
+        assertThat(result.get().getName()).isEqualTo(name);
+    }
+
+    @Test
+    void should_do_nothing_when_update_duplicates_value() {
+        // given
+        var givenId = UUID.randomUUID();
+        var updatedId = DataProvider.CONTACT_ID;
+        String name = faker.dog().name();
+        var givenRecord = Contact.builder().contactId(givenId).name(name).build();
+        var updatedRecord = Contact.builder().contactId(updatedId).name(name).build();
+
+        // when
+        repository.add(givenRecord);
+        repository.update(updatedRecord);
+        var result = repository.contactById(updatedId);
+
+        // then
+        assertThat(result).isNotEmpty();
+        assertThat(result.get().getName()).isNotEqualTo(name);
+    }
+
+    @Test
+    void should_do_nothing_when_record_not_exists() {
+        // given
+        var givenId = UUID.randomUUID();
+        String name = faker.cat().name();
+        var givenRecord = Contact.builder().contactId(givenId).name(name).build();
+
+        // when
+        repository.update(givenRecord);
+        var result = repository.contactById(givenId);
+
+        // then
+        assertThat(result).isEmpty();
+    }
 
 }
