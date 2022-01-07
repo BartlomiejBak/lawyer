624c6ccd3f72c094b9c7388be34ce52b1cc13755
BartlomiejBak <bakbartlomiej88@gmail.com>
2022-01-07 14:21:10 +0100

    test(task): test add and update task

--- src/test/java/pl/bartekbak/lawyer/repository/jooq/TaskRepositoryImplTest.java
+++ src/test/java/pl/bartekbak/lawyer/repository/jooq/TaskRepositoryImplTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.bartekbak.lawyer.common.PostgreSQLJooqContainer;
+import pl.bartekbak.lawyer.entity.Task;
 import pl.bartekbak.lawyer.repository.DataProvider;
 
 import java.util.UUID;
@@ -87,6 +88,72 @@ class TaskRepositoryImplTest {
         // then
         assertThat(result).isEmpty();
     }
+
+    @Test
+    void should_add_record_when_not_exists() {
+        // given
+        var givenId = UUID.randomUUID();
+        String description = faker.dog().name();
+        var givenRecord = Task.builder().taskId(givenId).description(description).build();
+
+        // when
+        repository.add(givenRecord);
+        var result = repository.taskById(givenId);
+
+        // then
+        assertThat(result).isPresent();
+        assertThat(result.get().getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    void should_do_nothing_when_adding_non_unique_value() {
+        // given
+        var givenId = UUID.randomUUID();
+        var additionalId = UUID.randomUUID();
+        String description = faker.dog().name();
+        var givenRecord = Task.builder().taskId(givenId).description(description).build();
+        var duplicateRecord = Task.builder().taskId(additionalId).description(description).build();
+
+        // when
+        repository.add(givenRecord);
+        repository.add(duplicateRecord);
+        var result = repository.taskById(additionalId);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void should_update_value_when_record_exists() {
+        // given
+        var givenId = DataProvider.TASK_ID;
+        String description = faker.cat().name();
+        var givenRecord = Task.builder().taskId(givenId).description(description).build();
+
+        // when
+        repository.update(givenRecord);
+        var result = repository.taskById(givenId);
+
+        // then
+        assertThat(result).isNotEmpty();
+        assertThat(result.get().getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    void should_do_nothing_when_record_not_exists() {
+        // given
+        var givenId = UUID.randomUUID();
+        String description = faker.cat().name();
+        var givenRecord = Task.builder().taskId(givenId).description(description).build();
+
+        // when
+        repository.update(givenRecord);
+        var result = repository.taskById(givenId);
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
 
 
 }
